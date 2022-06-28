49ed00364e6f48fa83fad3d5e76154438039773a
MarcosLazarte <marcos.e.lazarte@gmail.com>
2022-06-27 23:03:24 -0300

    primera pag terminada

--- src/pages/presentacion/Presentacion.jsx
+++ src/pages/presentacion/Presentacion.jsx
@@ -7,14 +7,14 @@ const Presentacion = () => {
         <section className='primeraPagina'>
           <div>
             <div className='primerPagina_titulo'>
-
               <div className='primeraPagina_titulo-letras'>
-                <h1 className='primeraPagina_titulo-letras1Fila'>Floop</h1>
-                <h1 className='primeraPagina_titulo-letras2Fila'>foOp</h1>
-                <h1 className='primeraPagina_titulo-letras3Fila'>fOop</h1>
-                <h1 className='primeraPagina_titulo-letras4Fila'>fooP</h1>
+                <h1 className='primeraPagina_titulo-letras1Fila'><span className='primeraPagina_titulo-letraImp'>F</span>oop</h1>
+                <h1 className='primeraPagina_titulo-letras2Fila'>fo<span className='primeraPagina_titulo-letraImp'>O</span>p</h1>
+                <h1 className='primeraPagina_titulo-letras3Fila'>f<span className='primeraPagina_titulo-letraImp'>O</span>op</h1>
+                <h1 className='primeraPagina_titulo-letras4Fila'>foo<span className='primeraPagina_titulo-letraImp'>P</span></h1>
               </div>
-
+              <div className='linea'></div>
+              <h1 className='primeraPagina_subTitulo'>games</h1>
             </div>
           </div>
           <div className='cabeza'>
@@ -26,7 +26,9 @@ const Presentacion = () => {
             <div className='cuernoIzquierdo-B'></div>
               <div className='ojos'>
                 <div className='ojo_izq'></div>
+                <div className='ojo_sombrasIzq'></div>
                 <div className='ojo_der'></div>
+                <div className='ojo_sombrasDer'></div>
               </div>
           </div>
         </section>
